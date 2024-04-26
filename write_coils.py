4e68c64fa3216fce053661aa498c526fd667a17a
Burak Eraslan <80619484+MrBrkers@users.noreply.github.com>
2024-04-26 16:50:26 +0300

    Create write_coils.py

---
+++ write_coils.py
@@ -0,0 +1,22 @@
+
+import time
+from pymodbus.client.sync import ModbusSerialClient
+modbus = ModbusSerialClient(method='rtu', port='COM7', baudrate=9600, parity ='N', bytesize = 8, stopbits=1,unit=1)
+modbus.connect()    
+if not modbus.connect() :
+    print("baglidegil")
+if modbus.connect() :
+    #herhangi bir M bitini setlemek icin
+    print("bagli")
+    while modbus.connect():
+        at = int(input("1 mi 0 mi : "))
+        if at == 1 :
+            result = modbus.write_coils(address = 2005,count=1,values=1,unit=1)
+            print(result)
+    
+            time.sleep(2)
+        if at == 0 :
+            result = modbus.write_coils(address = 2005,count=2,values=0,unit=1)
+            print(result)
+            
+            time.sleep(2)
